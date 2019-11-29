47ffac0e7fa745a027f31e3bc35e7c2451ee5935
bysiakhubert <bysiakhubert@gmail.com>
2019-11-29 16:36:35 +0100

    Configure spring security (#1)
    * enabled basic spring security
    
    * Refactoring
    
    * Refactoring

---
+++ src/main/java/virtus/lab/workshops/KotlinWorkshops/security/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package virtus.lab.workshops.KotlinWorkshops.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+//    TODO [hbysiak] this will be replaced by fetching data from the database once we add one
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
