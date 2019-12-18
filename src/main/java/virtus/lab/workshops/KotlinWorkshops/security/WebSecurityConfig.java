19944a6153c6c68f0891d7b4db486b0d142da48b
PLE12366530 <grzegorz.szymczuch@tesco.com>
2019-12-18 21:39:12 +0100

    Authenticate user on login page by users from database
    - turn on debug logging for now
    - user BCrypt password encoder

--- src/main/java/virtus/lab/workshops/KotlinWorkshops/security/WebSecurityConfig.java
+++ src/main/java/virtus/lab/workshops/KotlinWorkshops/security/WebSecurityConfig.java
@@ -1,18 +1,38 @@
 package virtus.lab.workshops.KotlinWorkshops.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private DataSource dataSource;
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        Objects.requireNonNull(dataSource, "Expected non-null dataSource");
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -22,19 +42,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login.html")
                 .permitAll();
-    }
-
-//    TODO [hbysiak] this will be replaced by fetching data from the database once we add one
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,21 @@
-DROP TABLE IF EXISTS users;
- 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  login VARCHAR(20) NOT NULL,
-  password VARCHAR(20) NOT NULL,
-  active boolean DEFAULT FALSE
+     username VARCHAR(50) NOT NULL, 
+     password VARCHAR(68) NOT NULL, 
+     enabled  BOOLEAN NOT NULL, 
+     PRIMARY KEY(username) 
 );
- 
-INSERT INTO users(login, password, active) VALUES ('admin', 'YWRtaW4K', TRUE);
+
+CREATE TABLE authorities (
+     username  VARCHAR(50) NOT NULL, 
+     authority VARCHAR(68) NOT NULL, 
+     FOREIGN KEY (username) REFERENCES users(username) 
+);
+--------------------------------
+------- insert data section ----
+--------------------------------
+-- username: admin, password: admin
+INSERT INTO users (username, password, enabled)
+VALUES ('admin', '$2y$12$eriM5C1/zX/X.3k3Fnz1luX7tGWB4K6HgASwXf5owapJzsVqg7Opu', 1);
+
+INSERT INTO authorities 
+VALUES ('admin', 'ADMIN');
