39a38f4c7f805b4e188d07c4cbe7c6f1b96b2538
Hubert Bysiak <hbysiak@virtuslab.com>
2019-12-21 18:35:49 +0100

    Added registration form

---
+++ src/main/java/virtus/lab/workshops/KotlinWorkshops/controller/RegistrationController.java
@@ -0,0 +1,18 @@
+package virtus.lab.workshops.KotlinWorkshops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import virtus.lab.workshops.KotlinWorkshops.model.UserDto;
+
+@Controller
+public class RegistrationController {
+    @RequestMapping(value = "registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+}
